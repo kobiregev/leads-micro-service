92640de0a3b96e2bb45b5d0509a88d7ecb39196c
kobiregev <regev79@gmail.com>
2023-01-04 13:49:14 +0200

    feat: with cors

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,9 +1,14 @@
 import fastify from 'fastify';
+import cors from '@fastify/cors';
+
 import { facebookRoute } from '../modules/facebook/facebook.route';
 
 export async function createServer() {
   const app = fastify();
-
+  app.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  });
   app.register(facebookRoute, { prefix: '/api/facebook' });
 
   return app;
