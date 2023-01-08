62bcb93e2aca70a4cd5562e5ea451eadd4cb6907
kobiregev <regev79@gmail.com>
2023-01-08 11:29:52 +0200

    fix: tsconfig and caddy config

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,14 +1,30 @@
 import fastify from 'fastify';
 import cors from '@fastify/cors';
-
+import swagger from '@fastify/swagger';
 import { facebookRoute } from '../modules/facebook/facebook.route';
-
+import { version } from '../../package.json';
 export async function createServer() {
   const app = fastify();
+
   app.register(cors, {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   });
+
+  // app.register(swagger, {
+  //   prefix: '/docs',
+  //   swagger: {
+  //     tags: [{ name: 'facebook' }],
+  //     info: {
+  //       title: 'Facebook',
+  //       description: 'Create subscription to facebook',
+  //       version,
+  //     },
+  //   },
+  //   staticCSP: true,
+  //   exposeRoute: true,
+  // });
+
   app.register(facebookRoute, { prefix: '/api/facebook' });
 
   return app;
